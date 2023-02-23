6e95a6851fb97a82a9651128280d60be437c4896
aytnkk <aydemk7665@gmail.com>
2023-02-23 10:24:51 +0100

    ba

---
+++ src/test/java/day08_ExplicitlyWait_cookies_webtables/C03_ExplicitlyWait.java
@@ -0,0 +1,17 @@
+package day08_ExplicitlyWait_cookies_webtables;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class C03_ExplicitlyWait {
+
+}
